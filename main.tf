f9d8cca597993974deda7e68720df231cc05a436
alissonmuniz17 <alissonmuniz17@gmail.com>
2025-05-29 16:57:33 -0300

    alterando instancia

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-075686beab831bb7f"
   instance_type = "t2.micro"
   key_name      = "iac-alura"
+  user_data = <<-EOF
+                #!/bin/bash
+            cd /home/ubuntu
+            echo "<h1>Feito com Terraform</h1>" > index.html
+            nohup busybox httpd -f -p 8080 &
+                EOF
 
   tags = {
-    Name = "primeira-instancia"
+    Name = "Teste aws"
   }
 }
